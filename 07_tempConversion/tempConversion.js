ffa5a3adf6dbd6c0a9c652c96f8a8d211a787671
sntovas <dovasstefanos@gmail.com>
2024-06-24 16:07:19 +0300

    finished 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  num = (num - 32)*5/9;
+  num = Math.round(num * 10) / 10;
+  return num;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  num = num*9/5 + 32;
+  num = Math.round(num * 10) / 10;
+  return num;
 };
 
 // Do not edit below this line
