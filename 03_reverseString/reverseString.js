2ba7bb0e30564443ec59c64a2e5fae2238a02582
sntovas <dovasstefanos@gmail.com>
2024-06-24 15:52:42 +0300

    first 5

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let final = [];
+    for(let i = 0; i < str.length; i++) {
+        final[i] = str.charAt(str.length - i - 1);
+    }
+    final = final.join("");
+    return final;
 };
 
 // Do not edit below this line
